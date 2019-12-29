d08fcba1f5ac9cb707d34e1f12a82adae85c6ae7
Michael D. Perini <perinimichaeld@yahoo.com>
2019-12-29 18:36:43 -0500

    completed

--- src/components/AddPoll.js
+++ src/components/AddPoll.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddPoll } from '../actions/polls'
 
-export default class AddPoll extends Component {
+class AddPoll extends Component {
     state = {
         question: '',
         a: '',
@@ -28,6 +30,7 @@ export default class AddPoll extends Component {
         e.preventDefault()
         // Redirect to /
         console.log('Add poll:', this.state)
+        this.props.dispatch(handleAddPoll(this.state))
     }
     render() {
         const { question, a, b, c, d } = this.state
@@ -38,7 +41,7 @@ export default class AddPoll extends Component {
                 <input
                     value={question}
                     onChange={this.handleInputChange}
-                    name={question}
+                    name='question'
                     className='input'
                     type='text'
                 />
@@ -49,37 +52,37 @@ export default class AddPoll extends Component {
                 <input
                     value={a}
                     onChange={this.handleInputChange}
-                    name={a}
+                    name='a'
                     className='input'
                     id='a'
                     type='text'
                 />
 
-<label className='label' htmlFor='b'>B.</label>
+                <label className='label' htmlFor='b'>B.</label>
                 <input
                     value={b}
                     onChange={this.handleInputChange}
-                    name={b}
+                    name='b'
                     className='input'
                     id='b'
                     type='text'
                 />
 
-<label className='label' htmlFor='c'>C.</label>
+                <label className='label' htmlFor='c'>C.</label>
                 <input
                     value={c}
                     onChange={this.handleInputChange}
-                    name={c}
+                    name='c'
                     className='input'
                     id='c'
                     type='text'
                 />
 
-<label className='label' htmlFor='d'>D.</label>
+                <label className='label' htmlFor='d'>D.</label>
                 <input
                     value={d}
                     onChange={this.handleInputChange}
-                    name={d}
+                    name='d'
                     className='input'
                     id='d'
                     type='text'
@@ -92,3 +95,5 @@ export default class AddPoll extends Component {
         )
     }
 }
+
+export default  connect()(AddPoll)
