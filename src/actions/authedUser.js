b410dacb176f4c5d4a02479541bf8a6e83be056e
Michael D. Perini <perinimichaeld@yahoo.com>
2019-12-29 11:46:52 -0500

    reducers added, store created

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,10 +1,10 @@
-import { S } from "xmlchars/xml/1.0/ed5";
-
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
 
-export const setAuthedUser = (id) => {
+const setAuthedUser = (id) => {
     return {
         type: SET_AUTHED_USER,
         id
     }    
 }
+
+export default setAuthedUser
