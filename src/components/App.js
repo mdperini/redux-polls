e1e4019546a19488ae7048bc4ac3706d1dff0488
Michael D. Perini <perinimichaeld@yahoo.com>
2019-12-29 13:41:30 -0500

    coded LoadingBar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import Loadingbar from 'react-redux-loading'
 
 class App extends Component {
   componentDidMount () {
@@ -10,8 +11,9 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Loadingbar />
         {this.props.loading === true
-          ? null
+          ? <h3>Loading...</h3>
           : <Dashboard />}
       </div>
     )
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import authedUser from './authedUser'
 import users from './users'
 import polls from './polls'
 import { loadingBarReducer } from 'react-redux-loading'
+
 export default combineReducers({
     authedUser,
     users,
