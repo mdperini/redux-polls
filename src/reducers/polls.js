3104db818afbfd3e356bc7d6a00054ba07797c5f
Michael D. Perini <perinimichaeld@yahoo.com>
2019-12-30 16:32:32 -0500

    completed

--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -1,4 +1,5 @@
-import { RECEIVE_POLLS } from '../actions/polls'
+import { RECEIVE_POLLS, ADD_POLL } from '../actions/polls'
+import { ADD_ANSWER} from '../actions/answers'
 
 const polls = (state={}, action) => {
     switch(action.type) {
@@ -6,6 +7,23 @@ const polls = (state={}, action) => {
             return {
                 ...state,
                 ...action.polls
+            }
+        case ADD_POLL:
+            return {
+                ...state,
+                [action.poll.id]: action.poll
+            }
+        case ADD_ANSWER:
+            const { answer, id, authedUser} = action
+            const poll = state[id]
+            const voteKey = answer + 'Votes'
+
+            return {
+                ...state,
+                [action.id]: {
+                    ...poll,
+                    [voteKey]: poll[voteKey].concat([authedUser])
+                }
             }
         default:
             return state
