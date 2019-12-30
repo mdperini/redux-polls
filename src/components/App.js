fb126aa86917596b97f884df0c6ec956e7de5c7d
Michael D. Perini <perinimichaeld@yahoo.com>
2019-12-30 16:58:16 -0500

    working example

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
@@ -6,6 +7,7 @@ import Loadingbar from 'react-redux-loading'
 import Leaderboard from './LeaderBoard'
 import AddPoll from './AddPoll'
 import Poll from './Poll'
+import Nav from './Nav'
 
 class App extends Component {
   componentDidMount () {
@@ -13,12 +15,22 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <Loadingbar />
-        {this.props.loading === true
-          ? <h3>Loading...</h3>
-          : <Poll match={{params: {id: 'xj352vofupe1dqz9emx13r'}}} />}
-      </div>
+      <Router>
+        <Fragment>
+          <Loadingbar />
+          <div className='container'>
+            <Nav />
+            {this.props.loading === true
+              ? null
+              : <div>
+                  <Route path='/' exact component={Dashboard} />
+                  <Route path='/leaderboard' component={Leaderboard} />
+                  <Route path='/polls/:id' component={Poll} />
+                  <Route path='/add' component={AddPoll} />
+              </div>}
+          </div>
+        </Fragment>
+      </Router>
     )
   }
 }
